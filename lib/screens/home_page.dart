fc739e494898586ec4234e7a366f3fc552405b4e
FanonX Rogers <fanonxdevelops@gmail.com>
2020-03-24 19:25:47 -0400

    Updated homepage with banner.

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:zengage_learning_platform/widgets/footer/footer.dart';
+import 'package:zengage_learning_platform/widgets/landing_banner/HeaderBanner.dart';
 import 'package:zengage_learning_platform/widgets/navbar/navbar.dart';
 
 /// Home page placeholder
@@ -23,6 +24,10 @@ class _HomePageState extends State<HomePage> {
             children: <Widget>[
               Column(
                 children: <Widget>[
+                  buildHeaderBanner(
+                      "assets/images/homepage/hp-banner-1.jpg",
+                      "THIS IS WHERE YOU START THINKING DIFFERENT \n TRAIN, TRANSFORM, PERFORM AND EVOlVE",
+                      context),
                   Footer(),
                 ],
               ),
