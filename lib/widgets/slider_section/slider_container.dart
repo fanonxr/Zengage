8242835a867301dc37f9e799635dc5b833db98cd
FanonX Rogers <fanonxdevelops@gmail.com>
2020-03-25 17:40:31 -0400

    Create image slider section.

---
+++ lib/widgets/slider_section/slider_container.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:getflutter/components/carousel/gf_carousel.dart';
+import 'package:zengage_learning_platform/widgets/mission_container/MissionContainer.dart';
+import 'package:zengage_learning_platform/widgets/partners_section/PartnerContainer.dart';
+import 'package:zengage_learning_platform/widgets/serivces_section/SeviceContainer.dart';
+
+buildSliderContainer(context) {
+  return Container(
+    height: MediaQuery.of(context).size.height / 1.5,
+    child: GFCarousel(
+      autoPlay: true,
+      aspectRatio: 1 / .35,
+      viewportFraction: 1.0,
+      enlargeMainPage: true,
+      pagination: true,
+      items: <Widget>[
+        MissionContainer(imagePath: "assets/images/homepage/hp-banner-2.jpg"),
+        ServiceContainer(),
+        PartnersContainer(),
+      ],
+      onPageChanged: (index) {},
+    ),
+  );
+}
