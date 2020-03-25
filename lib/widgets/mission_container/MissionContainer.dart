45901b9e124e4aa4c4b2f0cbdf2921ef3e42908e
FanonX Rogers <fanonxdevelops@gmail.com>
2020-03-25 17:38:42 -0400

    Improved mission section

--- lib/widgets/mission_container/MissionContainer.dart
+++ lib/widgets/mission_container/MissionContainer.dart
@@ -8,17 +8,18 @@ class MissionContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.only(left: 50.0),
       height: MediaQuery.of(context).size.height / 2,
       alignment: Alignment.center,
       decoration: BoxDecoration(
           image:
               DecorationImage(image: AssetImage(imagePath), fit: BoxFit.cover)),
       child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               MissionButton(
@@ -32,13 +33,31 @@ class MissionContainer extends StatelessWidget {
               )
             ],
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                  "Our Mission \n We Will guide you and your organzation in your quest to implement an agile environment. We will help you...")
-            ],
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Our Mission:",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w800),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 4,
+                  height: MediaQuery.of(context).size.height / 4,
+                  child: Text(
+                    "We will guide you and your organzation in your quest to implement an Agile environment. We will help you assess your current state, provide training to elevate your knowledge of Agile tools and practices, and offer individualized coaching as you roll out Agile. We are dedicated to helping you succeed!",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500),
+                  ),
+                )
+              ],
+            ),
           )
         ],
       ),
@@ -54,7 +73,7 @@ class MissionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width / 3,
+      width: MediaQuery.of(context).size.width / 4,
       child: Padding(
         padding: EdgeInsets.symmetric(vertical: 16.0),
         child: Material(
