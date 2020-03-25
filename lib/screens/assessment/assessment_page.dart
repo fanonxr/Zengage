daef7af390141658f4516f095df4132913ec5398
Adit Modhvadia <dev.aditmodhvadia@gmail.com>
2020-03-25 17:26:10 -0400

    - add contact us button in bottom of AssessmentPage

--- lib/screens/assessment/assessment_page.dart
+++ lib/screens/assessment/assessment_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:zengage_learning_platform/widgets/body/ContactUsImageBanner.dart';
 import 'package:zengage_learning_platform/widgets/footer/footer.dart';
 import 'package:zengage_learning_platform/widgets/navbar/navbar.dart';
-import 'package:zengage_learning_platform/widgets/body/ContactUsImageBanner.dart';
 
 import 'widgets/AssessmentsList.dart';
 import 'widgets/WhyAssessmentHeader.dart';
@@ -20,11 +20,41 @@ class _AssessmentPageState extends State<AssessmentPage> {
         body: Container(
           child: ListView(
             children: [
-              ContactUsImageBanner(),
-              WhyAssessmentHeader(),
-              AssessmentsList(),
+              Column(
+                children: <Widget>[
+                  ContactUsImageBanner(),
+                  WhyAssessmentHeader(),
+                  AssessmentsList(),
+                  ContactUsButton(),
+                ],
+              ),
               Footer(),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ContactUsButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 60.0),
+      width: MediaQuery.of(context).size.width / 3,
+      child: MaterialButton(
+        color: Colors.grey[300],
+        padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+        onPressed: () {
+          print("Contact Us clicked");
+        },
+        child: Text(
+          'Contact Us',
+          style: TextStyle(
+            fontSize: 22.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.lightBlue,
           ),
         ),
       ),
