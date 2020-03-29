3358fca8268376035c8a6d4aad50cd927179b20d
Adit Modhvadia <dev.aditmodhvadia@gmail.com>
2020-03-29 14:52:14 -0400

    - make body dropdown item list stateful to handle dropdown

--- lib/widgets/body/BodyDropdownItemsList.dart
+++ lib/widgets/body/BodyDropdownItemsList.dart
@@ -37,21 +37,53 @@ class BodyDropdownItemsList extends StatelessWidget {
   }
 }
 
-class AssessmentListItem extends StatelessWidget {
+class AssessmentListItem extends StatefulWidget {
   final String title;
 
   const AssessmentListItem({Key key, @required this.title}) : super(key: key);
 
   @override
+  _AssessmentListItemState createState() =>
+      _AssessmentListItemState(title: title);
+}
+
+class _AssessmentListItemState extends State<AssessmentListItem> {
+  String title;
+
+  _AssessmentListItemState({this.title});
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 40.0, bottom: 40.0),
-      child: Container(
-        color: Colors.grey[300],
-        padding: EdgeInsets.fromLTRB(16.0, 26.0, 0.0, 26.0),
-        child: Text(
-          title,
-          style: TextStyle(color: Colors.black45, fontWeight: FontWeight.bold),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            title = "Changed";
+          });
+        },
+        child: Container(
+          color: Colors.grey[300],
+          padding: EdgeInsets.fromLTRB(16.0, 26.0, 0.0, 26.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                title,
+                style: TextStyle(
+                    color: Colors.black45, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Icon(
+                  Icons.add,
+                  semanticLabel: 'Expand',
+                  color: Colors.lightBlue,
+                  size: 44.0,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
