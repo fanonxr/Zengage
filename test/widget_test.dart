eab4d23eb2b82708484e4ef5092ceefe51569019
Adit Modhvadia <dev.aditmodhvadia@gmail.com>
2020-04-01 03:01:22 -0400

    - remove redundant test case

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,11 +10,11 @@ import 'package:zengage_learning_platform/constants/app_constants.dart';
 import 'package:zengage_learning_platform/main.dart';
 
 void main() {
-  testWidgets('Loads Copyright', (WidgetTester tester) async {
+  /*testWidgets('Loads Copyright', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(ZengageApp());
 
 //    Verify that copyright text loads
     expect(find.text(copyrightText), findsOneWidget);
-  });
+  });*/
 }
