adef2e8a1baa4f626aefa19d7d525d0febbc7ea1
Adit Modhvadia <dev.aditmodhvadia@gmail.com>
2020-04-01 01:12:32 -0400

    Page-Registration (#13)
    - add registration form
    - add reset and a register button
    - add app_colors file

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 final Color textGreyColor = Color(0xff6B6B7E);
 
 final Color bgGreyColor = Color(0xffEDEDF5);
+
+final Color darkGreyColor = Color(0xff6B6B7E);
