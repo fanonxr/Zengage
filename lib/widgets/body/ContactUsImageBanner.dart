c3517089f7c018d7fff5025727189e42e09f1b1d
Adit Modhvadia <dev.aditmodhvadia@gmail.com>
2020-04-05 20:27:13 -0400

    - make BlueBorderWhiteButton widget reusable - remove explicit margins

--- lib/widgets/body/ContactUsImageBanner.dart
+++ lib/widgets/body/ContactUsImageBanner.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:zengage_learning_platform/constants/app_colors.dart';
 
 class ContactUsImageBanner extends StatelessWidget {
   final String imageLink;
@@ -25,33 +26,55 @@ class ContactUsImageBanner extends StatelessWidget {
               image: DecorationImage(
                   image: AssetImage(imageLink), fit: BoxFit.cover)),
         ),
-        Container(
-          margin:
-              EdgeInsets.only(left: imageWidth * 0.1, top: imageHeight * 0.35),
-          color: isButtonTranslucent
-              ? Colors.white.withOpacity(0.25)
-              : Colors.white,
-          child: OutlineButton(
-            padding: EdgeInsets.fromLTRB(imageWidth * 0.1, imageHeight * 0.03,
-                imageWidth * 0.1, imageHeight * 0.03),
-            child: Text(
-              "Contact Us",
-              style: TextStyle(
-                  fontSize: (imageWidth > 700.0) ? 18.0 : 12.0,
-                  color: isButtonTranslucent ? Colors.white : Colors.grey,
-                  fontWeight: FontWeight.bold),
-            ),
-            onPressed: () {
-              print("Contact Us pressed");
-            },
-            borderSide: BorderSide(
-              color: Colors.lightBlue, //Color of the border
-              style: BorderStyle.solid, //Style of the border
-              width: 4, //width of the border
-            ),
-          ),
-        )
+        BlueBorderWhiteButton(width: imageWidth, height: imageHeight)
       ],
+    );
+  }
+}
+
+class BlueBorderWhiteButton extends StatelessWidget {
+  BlueBorderWhiteButton({
+    Key key,
+    double width = -1.0,
+    double height = -1.0,
+  }) : super(key: key) {
+    this.width = width;
+    this.height = height;
+  }
+
+  double width;
+  double height;
+
+  @override
+  Widget build(BuildContext context) {
+    if (width == -1.0) {
+      width = MediaQuery.of(context).size.width;
+    }
+    if (height == -1.0) {
+      height = MediaQuery.of(context).size.height;
+    }
+    return Container(
+//      margin: EdgeInsets.only(left: width * 0.1, top: height * 0.35),
+      color: Colors.white,
+      child: OutlineButton(
+        padding: EdgeInsets.fromLTRB(
+            width * 0.1, height * 0.03, width * 0.1, height * 0.03),
+        child: Text(
+          "Contact Us",
+          style: TextStyle(
+              fontSize: (width > 700.0) ? 18.0 : 12.0,
+              color: textGreyColor,
+              fontWeight: FontWeight.bold),
+        ),
+        onPressed: () {
+          print("Contact Us pressed");
+        },
+        borderSide: BorderSide(
+          color: Colors.lightBlue, //Color of the border
+          style: BorderStyle.solid, //Style of the border
+          width: 4, //width of the border
+        ),
+      ),
     );
   }
 }
